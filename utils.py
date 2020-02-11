fdde97da4371a85e5ed02066db868af126f50e98
remcosurtel <remcosurtel@hotmail.com>
2020-02-11 20:09:14 +0100

    Add Point class

--- utils.py
+++ utils.py
@@ -15,3 +15,43 @@ def time_diff(start, end=None):
     if not end:
         end = datetime.utcnow()
     return int((end - start).total_seconds()*1000)
+
+
+"""
+Point object which stores the coordinates x and y
+
+x : float
+    x-coordinate
+y : float
+    y-coordinate
+"""
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def __repr__(self):
+        return f'({self.x}, {self.y})'
+    
+    # Point addition
+    def __add__(self, p: Point):
+        return Point(self.x + p.x, self.y + p.y)
+    
+    # Point subtraction
+    def __sub__(self, p: Point):
+        return Point(self.x - p.x, self.y - p.y)
+    
+    # Scalar multiplication
+    def __mul__(self, n: int):
+        return Point(self.x * n, self.y * n)
+    
+    # Scalar multiplication with self on the right-hand side
+    def __rmul__(self, n: int):
+        return Point(self.x * n, self.y * n)
+    
+    # Point negation
+    def __neg__(self):
+        return Point(-self.x, -self.y)
